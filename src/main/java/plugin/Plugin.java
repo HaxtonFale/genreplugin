b428cf0a9cee72df2257ca0a8045f09771ed2151
Haxton Fale <haxtonfale@gmail.com>
2018-03-19 15:45:48 +0000

    Check for and create database

--- src/main/java/plugin/Plugin.java
+++ src/main/java/plugin/Plugin.java
@@ -1,7 +1,33 @@
 package plugin;
 
+import java.io.File;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Plugin {
     public static void main(String[] args) {
+        String configPath = Paths.get(System.getProperty("user.home"), ".config").toString();
+        File configDir = new File(configPath);
+        if (!configDir.exists()) {
+            System.out.println("Creating .config directory in user's home directory...");
+            configDir.mkdir();
+            System.out.println(".config successfully created.");
+        }
 
+        String dbPath = Paths.get(configPath, "genreplugin.db").toString();
+        System.out.printf("Looking for sqlite database at %s...\n", dbPath);
+        String url = "jdbc:sqlite:" + dbPath;
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
